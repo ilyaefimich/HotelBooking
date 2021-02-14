4e92654f3631c31dbe8ff44097533a904656ac3f
ilyaefimich <ilyaefimich@gmail.com>
2021-02-14 13:28:22 +0300

    Add junit test for BookingDAO.

--- src/test/com/epam/jwd/finaltask/dao/impl/BookingDAOTest.java
+++ src/test/com/epam/jwd/finaltask/dao/impl/BookingDAOTest.java
@@ -1,18 +1,38 @@
 package test.com.epam.jwd.finaltask.dao.impl;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
 import java.util.ResourceBundle;
 
+/**
+ * Unit test which validates CRUD methods of the BookingDAO.
+ */
 @Test
 public class BookingDAOTest {
+    @BeforeClass
+    public void beforeClass() {
+        try {
+
+            String DRIVER = resourceBundle.getString("db.driver");
+
+            Class.forName(DRIVER).getDeclaredConstructor().newInstance();
+        } catch (IllegalAccessException | InstantiationException | ClassNotFoundException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private final static ResourceBundle resourceBundle = ResourceBundle.getBundle("resources.configuration.database");
+    private final static String URL = resourceBundle.getString("db.url");
+    private final static String LOGIN = resourceBundle.getString("db.login");
+    private final static String PASSWORD = resourceBundle.getString("db.password");
+
     private final static String SELECT_BOOKINGS =
-            "SELECT b.checkInDate, b.checkOutDate, b.adultsCount, b.roomTypeId, rt.name roomTypeName, b.bookingStatusId, bs.name bookingStatusName, \n" +
-                    "b.bookingId, b.childrenCount, b.comment, bs.name bookingStatusName, g.guestId, g.name guestName, g.mobile, g.email, g.address, \n" +
-                    "r.roomId, r.name roomName, r.roomStatusId, pm.paymentMethodId, pm.name paymentMethodName, pi.PaymentInfoId, pi.sum, pi.transactionId, pi.transactionDate, pi.transactionStatus \n" +
+            "SELECT COUNT(*)\n" +
                     "FROM booking b \n" +
                     "LEFT OUTER JOIN roomType rt ON rt.roomTypeId = b.roomTypeId \n" +
                     "LEFT OUTER JOIN bookingStatus bs ON bs.bookingStatusId = b.bookingStatusId \n" +
@@ -21,30 +41,25 @@ public class BookingDAOTest {
                     "LEFT OUTER JOIN paymentmethod pm ON pm.paymentMethodId = b.PreferedpaymentMethodId \n" +
                     "LEFT OUTER JOIN paymentinfo pi ON pi.PaymentInfoId= b.PaymentInfoId \n";
 
+    /**
+     * Tests the selection of bookings.
+     */
     @Test
     public void testGetBookings() {
+        int size = 0;
         try {
-            ResourceBundle resourceBundle = ResourceBundle.getBundle("resources.configuration.database");
-            String DRIVER = resourceBundle.getString("db.driver");
-            String URL = resourceBundle.getString("db.url");
-            String LOGIN = resourceBundle.getString("db.login");
-            String PASSWORD = resourceBundle.getString("db.password");
-
-            Class.forName(DRIVER).getDeclaredConstructor().newInstance();
-            Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-            PreparedStatement statement = connection.prepareStatement(SELECT_BOOKINGS);
-            ResultSet rs = statement.executeQuery();
-            if (rs != null) {
-                rs.beforeFirst();
-                rs.last();
-                int size = rs.getRow();
-                Assert.assertTrue(size > 0);
+            try (Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD)) {
+                try (PreparedStatement statement = connection.prepareStatement(SELECT_BOOKINGS)) {
+                    try (ResultSet rs = statement.executeQuery()) {
+                        while (rs.next()) {
+                            size = rs.getInt(1);
+                        }
+                    }
+                }
             }
-
-
-        } catch (SQLException | IllegalAccessException | InstantiationException | ClassNotFoundException | NoSuchMethodException | InvocationTargetException e) {
-            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        Assert.assertTrue(size > 0);
     }
-
 }
